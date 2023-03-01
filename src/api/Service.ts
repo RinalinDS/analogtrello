bd79b784bb74ac27179a89f5de80d8c6705a63fb
DenisPilyutin <denispilyutin@gmail.com>
2023-03-01 19:55:59 +0200

    refactor: using enums in paths

--- src/api/Service.ts
+++ src/api/Service.ts
@@ -1,33 +1,38 @@
 import axios, { AxiosResponse } from 'axios'
 
+import { ServicePath } from '../enums/ServicePath'
+
 import { BoardType, CardType, TaskType } from '../types/BoardsType'
 
+const baseURL = 'http://localhost:4000'
+
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL,
   headers: {},
 })
 
 export class Service {
   static async getBoards(): Promise<AxiosResponse<BoardType[]>> {
-    return instance.get('/boards')
+    return instance.get(ServicePath.boards)
   }
 
-  static async getCards(id: number): Promise<AxiosResponse<CardType[]>> {
-    return instance.get(`/boards/${id}?_embed=cards`)
+  static async getCardsByBoardId(id: number): Promise<AxiosResponse<CardType[]>> {
+    return instance.get(`${ServicePath.boards}/${id}?_embed=cards`)
   }
 
-  static async getTasks(id: number): Promise<AxiosResponse<TaskType[]>> {
-    return instance.get(`/cards/${id}?_embed=tasks`)
+  static async getTasksByCardsId(id: number): Promise<AxiosResponse<TaskType[]>> {
+    return instance.get(`${ServicePath.cards}/${id}?_embed=tasks`)
   }
 
   static async addBoard(board: BoardType): Promise<AxiosResponse<BoardType>> {
-    return instance.post('/boards', board)
+    return instance.post(ServicePath.boards, board)
   }
 
   static async addCard(card: CardType): Promise<AxiosResponse<CardType>> {
-    return instance.post('/cards', card)
+    return instance.post(ServicePath.cards, card)
   }
+
   static async addTask(task: TaskType): Promise<AxiosResponse<TaskType>> {
-    return instance.post('/tasks', task)
+    return instance.post(ServicePath.tasks, task)
   }
 }
