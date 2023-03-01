d75d812b26296e385dbcc597f937e9c91b3a38d5
DenisPilyutin <denispilyutin@gmail.com>
2023-03-01 19:54:38 +0200

    refactor: remove strictmode and css

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,13 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './App'
-import './index.css'
 import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 )
