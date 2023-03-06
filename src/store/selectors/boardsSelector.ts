9927454f316d84a52cd9cd3b5d4e81830e65ccc7
DenisPilyutin <denispilyutin@gmail.com>
2023-03-06 20:45:43 +0200

    feature: add selector for CurrentBoard

--- src/store/selectors/boardsSelector.ts
+++ src/store/selectors/boardsSelector.ts
@@ -1,3 +1,4 @@
 import { AppRootStateType } from '../index'
 
 export const selectBoards = (state: AppRootStateType) => state.boards.boards
+export const selectCurrentBoard = (state: AppRootStateType) => state.boards.currentBoard
