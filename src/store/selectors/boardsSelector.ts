356429ffa40809ef77bfeca46c7c0541cd35da17
DenisPilyutin <denispilyutin@gmail.com>
2023-03-14 19:54:20 +0200

    refactor: rename

--- src/store/selectors/boardsSelector.ts
+++ src/store/selectors/boardsSelector.ts
@@ -3,19 +3,19 @@ import { createSelector } from 'reselect'
 import { AppRootStateType } from '../index'
 
 export const selectBoards = (state: AppRootStateType) => state.boards.boards
-export const selectFlag = (state: AppRootStateType) => state.boards.newBoardCreated
+export const selectIsNewBoardCreated = (state: AppRootStateType) => state.boards.isNewBoardCreated
 export const selectCurrentBoardId = (state: AppRootStateType) => state.boards.currentBoardId
 
 export const selectCurrentBoard = createSelector(
   [selectBoards, (_, boardId: string | undefined) => ({ boardId })],
-  (cards, { boardId }) => {
+  (boards, { boardId }) => {
     if (!boardId) {
       return null
     }
-    const card = cards.find(f => f.id === +boardId)
-    if (!card) {
+    const board = boards.find(f => f.id === +boardId)
+    if (!board) {
       return null
     }
-    return card
+    return board
   },
 )
