44a4fb8a2f759ee2cda8e7b41baa4d11bf084528
DenisPilyutin <denispilyutin@gmail.com>
2023-03-14 20:02:00 +0200

    refactor: attempt on universal menu

--- src/common/Menu/index.tsx
+++ src/common/Menu/index.tsx
@@ -1,73 +1,58 @@
 import * as React from 'react'
-import { FC, memo } from 'react'
+import { FC, memo, useCallback } from 'react'
 import Button from '@mui/material/Button'
 import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
 import styled from 'styled-components'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
-import AddIcon from '@mui/icons-material/Add'
 
-import { LabelMessage } from '../../enums/Message'
-import { AddBoardForm } from '../AddItemForm/AddBoardForm'
+import { Icon, IconType } from '../Icon'
 
-type menuType = {
-  onClick: () => void
-  title: string
+type BasicMenuPropsType = {
+  iconType: IconType
+  children: React.ReactNode
+  setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
+  anchorEl: HTMLElement | null
 }
 
-export const BasicMenu: FC<{
-  menuList?: menuType
-  children?: React.ReactNode
-  plus?: boolean
-  addBoardHandler?: (title: string, color: string) => void
-}> = memo(({ menuList, children, plus, addBoardHandler }) => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-  const onClickHandler = () => {
-    handleClose()
-    menuList?.onClick()
-  }
+export const BasicMenu: FC<BasicMenuPropsType> = memo(
+  ({ iconType, children, setAnchorEl, anchorEl }) => {
+    const open = Boolean(anchorEl)
+    const handleClick = useCallback(
+      (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget)
+      },
+      [setAnchorEl],
+    )
 
-  const addBoard = (title: string, color: string) => {
-    addBoardHandler && addBoardHandler(title, color)
-    handleClose()
-  }
-  return (
-    <div>
-      <StyledMenuBtn
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        {plus ? (
-          <StyledAddIcon fontSize={'large'} color={'action'} />
-        ) : (
-          <StyledMoreIcon fontSize={'large'} color={'action'} />
-        )}
-      </StyledMenuBtn>
-      <StyledMenu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <StyledMenuItem onClick={onClickHandler}>{menuList?.title}</StyledMenuItem>
-        {plus && <AddBoardForm callBack={addBoard} label={LabelMessage.BoardTitle} />}
-      </StyledMenu>
-    </div>
-  )
-})
+    const handleClose = useCallback(() => {
+      setAnchorEl(null)
+    }, [setAnchorEl])
+
+    return (
+      <>
+        <StyledMenuBtn
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          <Icon type={iconType} />
+        </StyledMenuBtn>
+        <StyledMenu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          {children}
+        </StyledMenu>
+      </>
+    )
+  },
+)
 
 const StyledMenuBtn = styled(Button)`
   cursor: pointer;
@@ -83,12 +68,4 @@ const StyledMenuBtn = styled(Button)`
 `
 const StyledMenu = styled(Menu)`
   z-index: 5;
-`
-
-const StyledMoreIcon = styled(MoreHorizIcon)``
-const StyledAddIcon = styled(AddIcon)``
-const StyledMenuItem = styled(MenuItem)`
-  div & {
-    font-size: 1.4rem;
-  }
 `
